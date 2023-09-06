20d5666447da93d51c90837281656b38156f5b19
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2023-09-06 11:33:18 +0200

    refactor context and dispatchcontext to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { useEffect, useReducer } from "react";
+import { RestaurantReducer } from "./reducers/RestaurantReducer";
+import { IRestaurant } from "./models/IRestaurant";
+import { createRestaurant } from "./services/RestaurantService";
+import { RestaurantContext } from "./contexts/RestaurantContext";
+import { RestaurantDispatchContext } from "./contexts/RestaurantDispatchContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,12 +24,29 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [restaurant, dispatch] = useReducer(
+    RestaurantReducer,
+    {} as IRestaurant
+  );
+
+  useEffect(() => {
+    async function getData() {
+      const restaurant = await createRestaurant();
+      dispatch({ type: "SETRESTAURANT", payload: restaurant });
+    }
+    getData();
+  }, []);
+
   return (
     <html lang="en">
-     <body className={inter.className}>
-        <Header /> 
-        {children}
-        <Footer></Footer>
+      <body className={inter.className}>
+        <RestaurantContext.Provider value={restaurant}>
+          <RestaurantDispatchContext.Provider value={dispatch}>
+            <Header />
+            {children}
+            <Footer></Footer>
+          </RestaurantDispatchContext.Provider>
+        </RestaurantContext.Provider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,14 @@
 "use client";
 
-import { useEffect, useReducer, useState } from "react";
-import { ICreateRestaurant } from "./models/ICreateRestaurant";
-import styles from "./page.module.css";
-import axios from "axios";
-import { log } from "console";
 import { Hero } from "./components/Hero";
 import { About } from "./components/About";
-import { IRestaurant } from "./models/IRestaurant";
-import { createRestaurant } from "./services/RestaurantService";
-import { RestaurantReducer } from "./reducers/RestaurantReducer";
-import { RestaurantContext } from "./contexts/RestaurantContext";
-import { RestaurantDispatchContext } from "./contexts/RestaurantDispatchContext";
 
 //vi ska kalla på createrestaurant och få svar och svaret tillgängligt i hela appen
 export default function Home() {
-  const [restaurant, dispatch] = useReducer(
-    RestaurantReducer,
-    {} as IRestaurant
-  );
-
-  useEffect(() => {
-    async function getData() {
-      const restaurant = await createRestaurant();
-      dispatch({ type: "SETRESTAURANT", payload: restaurant });
-    }
-    getData();
-  }, []);
-
   return (
-    <RestaurantContext.Provider value={restaurant}>
-      <RestaurantDispatchContext.Provider value={dispatch}>
-        <Hero></Hero>
-        <About></About>
-      </RestaurantDispatchContext.Provider>
-    </RestaurantContext.Provider>
+    <>
+      <Hero></Hero>
+      <About></About>
+    </>
   );
 }
